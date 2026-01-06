f20b444c07bd527308629003b857a7bd8a5a6597
jbihamba <josephbihamba25@gmail.com>
2026-01-06 09:05:50 +0100

    feat(contact): add function to save a new contact via API

--- js/contact.js
+++ js/contact.js
@@ -7,6 +7,13 @@ const addContactBtn = document.getElementById("add-contact-btn");
 const contactModal = document.getElementById("contact-modal");
 // Bouton pour fermer la fenêtre modale
 const closeContactModalBtn = document.getElementById("close-contact-modal-btn");
+//modal elements
+const contactFullName = document.getElementById("contact-full-name");
+const contactEmail = document.getElementById("contact-email");
+const contactCompany = document.getElementById("contact-company");
+const contactCompanySector = document.getElementById("contact-sector");
+const contactAvatar = document.getElementById("contact-photo");
+const saveContactBtn = document.getElementById("save-contact-btn");
 
 
 // Gestion des événements
@@ -20,3 +27,34 @@ addContactBtn.addEventListener("click", () => toggleModal(true));
 //  ferme la fenêtre modale
 closeContactModalBtn.addEventListener("click", () => toggleModal(false));
 
+// Fonction pour enregistrer un nouveau contact
+async function saveContact() {
+        // Récupération des valeurs du formulaire
+        const newContact = {
+            fullName: contactFullName.value,
+            email: contactEmail.value,
+            company: contactCompany.value,
+            sector: contactCompanySector.value,
+            avatar: contactAvatar.value
+        };
+        try {
+            // Envoi de la requête POST à l’API
+            const response = await fetch(apiUrl, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(newContact)
+            });
+            if (response.ok) {
+                // Fermeture du modal après l'enregistrement
+                toggleModal(false);
+                // Actualisation de la liste des contacts
+                fetchContacts();
+            } else {
+                console.error("Error saving contact");
+            }
+        } catch (error) {
+            console.error("Error saving contact :", error);
+        }
+    }
